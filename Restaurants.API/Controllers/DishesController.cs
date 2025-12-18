8e35cdc219cbf56894aaad0d10deaa71e49bd2e5
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2025-12-18 12:45:22 +0100

    Update swagger attributes

--- Restaurants.API/Controllers/DishesController.cs
+++ Restaurants.API/Controllers/DishesController.cs
@@ -13,31 +13,37 @@ namespace Restaurants.API.Controllers;
 public class DishesController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateDish([FromRoute] int restaurantId, CreateDishCommand command)
     {
         command.RestaurantId = restaurantId;
         var dishId = await mediator.Send(command);
-        return CreatedAtAction("GetDishForRestaurant",new {restaurantId,dishId},null);
+        return CreatedAtAction("GetDishForRestaurant", new { restaurantId, dishId }, null);
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<DishDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IEnumerable<DishDto>>> GetAllDishesForRestaurant([FromRoute] int restaurantId)
     {
         var dishes = await mediator.Send(new GetDishesForRestaurantQuery(restaurantId));
         return Ok(dishes);
     }
 
-    [HttpGet("{dishId:int}",Name = "GetDishForRestaurant")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<DishDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet("{dishId:int}", Name = "GetDishForRestaurant")]
     public async Task<ActionResult<DishDto>> GetDishForRestaurant([FromRoute] int restaurantId, [FromRoute] int dishId)
     {
         var dish = await mediator.Send(new GetDishByIdForRestaurantQuery(restaurantId, dishId));
         return Ok(dish);
     }
 
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpDelete]
     public async Task<IActionResult> DeleteAllDishesForRestaurant([FromRoute] int restaurantId)
     {
-       
         await mediator.Send(new DeleteAllDishesForRestaurantCommand(restaurantId));
         return NoContent();
     }
